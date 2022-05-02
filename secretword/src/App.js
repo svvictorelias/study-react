96f752788d3328beea9c3123b2da0a5160792026
Victor Elias <svvictorelias@gmail.com>
2022-05-02 10:10:52 -0300

    buscando letras digitadas

--- secretword/src/App.js
+++ secretword/src/App.js
@@ -54,8 +54,8 @@ function App() {
         setGameStage(stages[1].name);
     };
 
-    const verifyLetter = () => {
-        setGameStage(stages[2].name);
+    const verifyLetter = (letter) => {
+        console.log(letter)
     };
     const retry = () => {
         setGameStage(stages[0].name);
