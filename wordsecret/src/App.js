f5b6d7ae035da22f47848e459fb1032cb646e5be
Victor Elias <svvictorelias@gmail.com>
2022-05-12 12:32:09 -0300

    criado telas, e funções de escolher categoria e palavra

--- wordsecret/src/App.js
+++ wordsecret/src/App.js
@@ -2,30 +2,63 @@
 import './App.css'
 
 //React
-import {useCallback, useEffect, useState} from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 //data
-import {wordsList} from './data/words'
+import { wordsList } from './data/words'
 
 //components
 import StartScreen from './components/StartScreen'
 import GameOver from './components/GameOver'
 import Game from './components/Game'
 
-const stages =[
-  {id:1, name:'start'},
-  {id:2, name:'game'},
-  {id:3, name:'end'},
+const stages = [
+  { id: 1, name: 'start' },
+  { id: 2, name: 'game' },
+  { id: 3, name: 'end' }
 ]
 
 function App() {
-  const [gameStage, setGameStage] =useState(stages[0].name)
+  const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
-  return <div className="App">
-    {gameStage==='start' && <StartScreen />}
-    {gameStage==='game' && <Game/>}
-    {gameStage==='end' && <GameOver />}
-  </div>
+
+  const [pickedWord, setPickedWord] = useState('')
+  const [pickedCategory, setPickedCategory] = useState('')
+  const [letters, setLetters] = useState([])
+
+  const picWordAndCategory = () => {
+    const randomType = typeData => {
+      return typeData[Math.floor(Math.random() * Object.keys(typeData).length)]
+    }
+    const categories = Object.keys(words)
+    //pick a random category
+    const category = randomType(categories)
+    //pick a random word
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)]
+
+    return [word, category]
+  }
+
+  const startGame = () => {
+    //pick word and pick category
+    const {word, category} = picWordAndCategory()
+    setGameStage(stages[1].name)
+  }
+
+  //process the letter input
+  const veifyLetter = () => {}
+
+  //restart game
+  const retry = () => {}
+
+  return (
+    <div className="App">
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
+    </div>
+  )
 }
 
 export default App
--- wordsecret/src/components/StartScreen.js
+++ wordsecret/src/components/StartScreen.js
@@ -1,11 +1,11 @@
 import './StartScreen.css'
 
-const StartScreen = () => {
+const StartScreen = ({startGame}) => {
   return (
     <div className="start">
       <h1>Secret Word</h1>
       <p>Clique no Botão para começar a jogar</p>
-      <button>Começar a Jogar</button>
+      <button onClick={startGame}>Começar a Jogar</button>
     </div>
   )
 }
